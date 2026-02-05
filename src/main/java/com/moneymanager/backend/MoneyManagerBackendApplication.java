955431faf85326fc6e75ad58dd544f908045597d
Jagadeesan v <venujagadeesan@gmail.com>
2026-02-05 13:52:18 +0530

    Initial commit

---
+++ src/main/java/com/moneymanager/backend/MoneyManagerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.moneymanager.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyManagerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneyManagerBackendApplication.class, args);
+	}
+
+}
